634e1b22569f8e5b2c5168c19f2c1d71531ef444
Danah <danah.kim@kakao.com>
2023-10-14 15:25:29 +0900

    style: 포켓몬 컬러 테마 추가, 타입 아이콘 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,90 @@ const config: Config = {
         forest: "url('/assets/images/forest.jpg')",
         glacier: "url('/assets/images/glacier.jpg')",
       },
+      colors: {
+        pokemon: {
+          normal: {
+            main: '#919AA2',
+            bg: '#F1F2F3',
+          },
+          fighting: {
+            main: '#CE416B',
+            bg: '#F8E9EE',
+          },
+          flying: {
+            main: '#89AAE3',
+            bg: '#F1F4FA',
+          },
+          poison: {
+            main: 'B567CE',
+            bg: '#F5EDF8',
+          },
+          ground: {
+            main: '#D97845',
+            bg: '#F9EFEA',
+          },
+          rock: {
+            main: '#C5B78C',
+            bg: '#F7F5F1',
+          },
+          bug: {
+            main: '#91C12F',
+            bg: '#F1F6E8',
+          },
+          ghost: {
+            main: '#5269AD',
+            bg: '#EBEDF4',
+          },
+          steel: {
+            main: '#5A8EA2',
+            bg: '#ECF1F3',
+          },
+          fire: {
+            main: '#FF9D55',
+            bg: '#FCF3EB',
+          },
+          water: {
+            main: '#5090D6',
+            bg: '#EBF1F8',
+          },
+          grass: {
+            main: '#63BC5A',
+            bg: '#EDF6EC',
+          },
+          electric: {
+            main: '#F4D23C',
+            bg: '#FBF8E9',
+          },
+          phychic: {
+            main: '#FA7179',
+            bg: '#FCEEEF',
+          },
+          ice: {
+            main: '#73CEC0',
+            bg: '#F1FBF9',
+          },
+          dragon: {
+            main: '#0B6DC3',
+            bg: '#E4EEF6',
+          },
+          dark: {
+            main: '#5A5465',
+            bg: '#ECEBED',
+          },
+          fairy: {
+            main: '#EC8FE6',
+            bg: '#FBF1FA',
+          },
+          unknown: {
+            main: '#333333',
+            bg: '#F1F2F3',
+          },
+          shadow: {
+            main: '#E6E6E6',
+            bg: '#F1F2F3',
+          },
+        },
+      },
     },
   },
   plugins: [],
