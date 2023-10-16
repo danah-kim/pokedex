f0df678251119b5e10c745cf6eeabb6511f4d445
Danah <danah.kim@kakao.com>
2023-10-16 13:52:21 +0900

    fix: 3D model에 반영 후 로드 되도록 위치 변경

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -29,19 +29,19 @@ const View: React.FC<ViewProps> = ({
         <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
           {children}
         </group>
+        <Environment preset="city" {...envOptions} />
+        {typeof orbitOptions !== 'undefined' ? (
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            minPolarAngle={Math.PI / 2.2}
+            maxPolarAngle={Math.PI / 2.2}
+            {...(typeof orbitOptions === 'boolean' ? null : orbitOptions)}
+          />
+        ) : null}
         <Suspense fallback={null}>
           <pointLight color={lightColor} position={[-5, 4, -15]} intensity={1.5} />
           <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
-          {typeof orbitOptions !== 'undefined' ? (
-            <OrbitControls
-              enablePan={false}
-              enableZoom={false}
-              minPolarAngle={Math.PI / 2.2}
-              maxPolarAngle={Math.PI / 2.2}
-              {...(typeof orbitOptions === 'boolean' ? null : orbitOptions)}
-            />
-          ) : null}
-          <Environment preset="city" {...envOptions} />
         </Suspense>
       </Suspense>
     </Canvas>
